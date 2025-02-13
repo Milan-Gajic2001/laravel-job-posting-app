920e8d28362906d0b1b5aa8a7fa6517a89afe3f3
Milan-Gajic2001 <156429209+Milan-Gajic2001@users.noreply.github.com>
2025-02-13 17:35:11 +0100

    fix route bug

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/register', [UserController::class, 'registrationForm'])->name('user
 Route::post('/register', [UserController::class, 'registerUser'])->name('registerUser')->middleware('guest');
 
 Route::get("/login", [UserController::class, 'loginForm'])->name('loginFormPage')->middleware('guest');
-Route::post("/login", [UserController::class, 'loginUser'])->name('loginUser')->middleware('guest');
+Route::post("/login", [UserController::class, 'loginUser'])->name('login')->middleware('guest');
 Route::get("/logout", [UserController::class, 'logout'])->name('logout')->middleware('guest');
 
 Route::get('/user/{id}', [UserController::class, 'userDetails'])->name('userDetails');
